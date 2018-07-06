3ee0bfe85c772e39fdbbbdbaf040586fc2a6c224
杨健锋 <80003058@sf80003058demac-mini.local>
2018-07-06 14:53:24 +0800

    制造冲突

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.green
-        print("branch")
+        print("test branch")
     }
 
     override func didReceiveMemoryWarning() {
