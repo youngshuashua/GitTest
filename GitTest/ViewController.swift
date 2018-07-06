bc2e6f8b62d27243e865b70e188bd03383efda7c
杨健锋 <80003058@sf80003058demac-mini.local>
2018-07-06 14:04:53 +0800

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by 杨健锋(80003058) on 2018/7/6.
+//  Copyright © 2018年 杨健锋(80003058). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
